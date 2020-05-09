986c8e9e1d05cfb8ebc1cbce4eb2ed1f530f23a1
sim511777 <sim51177@gmail.com>
2020-05-10 08:52:20 +0900

    public unsafe static -> public static unsafe

--- SoftwareRendering/Util.cs
+++ SoftwareRendering/Util.cs
@@ -13,7 +13,7 @@ namespace SoftwareRendering {
             return (double)Stopwatch.GetTimestamp() / Stopwatch.Frequency;
         }
 
-        public unsafe static void Memset(IntPtr dbuf, int val, int count) {
+        public static unsafe void Memset(IntPtr dbuf, int val, int count) {
             byte* dp = (byte*)dbuf;
             byte bval = (byte)val;
             for (int i = 0; i < count; i++, dp++) {
@@ -21,13 +21,14 @@ namespace SoftwareRendering {
             }
         }
 
-        public unsafe static void Memset4(IntPtr dbuf, int val, int count) {
+        public static unsafe void Memset4(IntPtr dbuf, int val, int count) {
             int* dp = (int*)dbuf;
             for (int i = 0; i < count; i++, dp++) {
                 *dp = val;
             }
         }
-        public unsafe static void Memcpy(IntPtr dbuf, IntPtr sbuf, int count) {
+
+        public static unsafe void Memcpy(IntPtr dbuf, IntPtr sbuf, int count) {
             byte* sp = (byte*)sbuf;
             byte* dp = (byte*)dbuf;
             for (int i = 0; i < count; i++, dp++) {
@@ -35,7 +36,7 @@ namespace SoftwareRendering {
             }
         }
 
-        public unsafe static void Memcpy4(IntPtr dbuf, IntPtr sbuf, int count) {
+        public static unsafe void Memcpy4(IntPtr dbuf, IntPtr sbuf, int count) {
             int* sp = (int*)sbuf;
             int* dp = (int*)dbuf;
             for (int i = 0; i < count; i++, dp++) {
